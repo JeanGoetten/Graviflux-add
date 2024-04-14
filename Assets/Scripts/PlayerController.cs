b0e233a42ce4dc45b9492d43144b874828d60fc9
@JeanGoetten <selbestdenkerdot@hotmail.com>
2024-04-13 23:18:14 -0300

    add new basic mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float horizontalInput;
 
+    private float aceleration; 
+
     public AudioSource au;
     public AudioSource au2;
-    public AudioClip[] clips; 
+    public AudioClip[] clips;
+
+    public GameObject portal_1;
+    public GameObject portal_2;
+
+    public float cdPortal = 1f;
+    private float timeRec;
+
+    private bool onTravel;
+
+    private float portalForceIncrease; 
 
     private void Awake()
     {
@@ -22,11 +34,21 @@ public class PlayerController : MonoBehaviour
         horizontalInput = 0;
 
         au2.PlayOneShot(clips[2]);
+
+        aceleration = 0;
+
+        timeRec = 0;
+
+        onTravel = false;
+
+        portalForceIncrease = 1;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        timeRec += Time.deltaTime; 
+
+        if (Input.GetKeyDown(KeyCode.Space) && !onTravel)
         {
             au.PlayOneShot(clips[0]);
             gravity *= -1;
@@ -51,12 +73,30 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
         }
+
+        if (transform.position.y > 18.5)
+        {
+            transform.position = new Vector3(transform.position.x, -8.5f, transform.position.z); 
+        }else if(transform.position.y < -8.5)
+        {
+            transform.position = new Vector3(transform.position.x, 18.5f, transform.position.z);
+        }
+
+        Debug.Log(aceleration); 
     }
 
     void FixedUpdate()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        if(onTravel)
+        {
+            
+        }
+        else
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        }
+        
 
     }
 
@@ -73,6 +113,44 @@ public class PlayerController : MonoBehaviour
             au.PlayOneShot(clips[1]);
             Reset();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (other.gameObject.tag == "Ground")
+        {
+            aceleration = 0f;
+        onTravel = false;
+        portalForceIncrease = 0f; 
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Portal_1")
+        {
+            if (timeRec > cdPortal)
+            {
+                onTravel = true;
+                timeRec = 0;
+                portalForceIncrease++;
+                aceleration += (rb.velocity.magnitude / 5) * portalForceIncrease;
+                transform.position = new Vector2(portal_2.transform.position.x + 1.5f, portal_2.transform.position.y + 1);
+                rb.velocity = new Vector2(aceleration, aceleration);
+                portalForceIncrease += 0.5f; 
+            }
+            
+            
+        }
+        if (other.gameObject.tag == "Portal_2")
+        {
+            if (timeRec > cdPortal)
+            {
+                onTravel = true;
+                timeRec = 0;
+                portalForceIncrease++;
+                aceleration += (rb.velocity.magnitude/5) * portalForceIncrease;
+                transform.position = new Vector2(portal_1.transform.position.x + 1.5f, portal_1.transform.position.y + 1);
+                rb.velocity = new Vector2(aceleration, aceleration);
+                
+            }
         }
     }
 }
